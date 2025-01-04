8a3410405aec2621105a313f8c38c29be27e5a91
Quoc Dung Le <dungel@M1.local>
2025-01-04 12:24:59 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,18 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        // switch case for input 
-        match input.trim() {
-            "exit 0" => break,
-            &_ => {
-                println!("{}: command not found", input.trim());
+        // Check for echo command 
+        if input.starts_with("echo ") {
+            let echo_cmd = input.trim_start_matches("echo ");
+            println!("{}", echo_cmd.trim());
+        } else {
+            // Cases for whole command
+            match input.trim() {
+                "exit" | "exit 0" => break,
+
+                &_ => {
+                    println!("{}: command not found", input.trim());
+                }
             }
         }
     }
